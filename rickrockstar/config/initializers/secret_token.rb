91b2175ddaef2b5ce4e076dbdd670e34ab4487c4
Matt Day <mday100@live.com>
2014-07-23 17:59:03 -0400

    refinerycms rickrockstar

---
+++ rickrockstar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rickrockstar::Application.config.secret_token = 'a5f5d3fd15dd2be0f77043761c90c96f484621c1e42007777814fab8983bedee4fc729fbd2de9ce53a689c4ed5d73207020436f96b0474ba5984e03448388de4'
